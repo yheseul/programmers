8324cf94a46e29ee36a17cebff59462ee2c5e44c
yheseul <111976264+yheseul@users.noreply.github.com>
2024-08-09 10:49:53 +0900

    [level 0] Title: 글자 지우기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,5 +1,3 @@
 function solution(my_string, indices) {
-  const arr = [...my_string]
-  indices.sort((a,b) => a-b).map((_, i) => arr.splice(indices[i] - i, 1))
-  return arr.join('')
+  return [...my_string].filter((_, i) => !indices.includes(i)).join('')
 }
