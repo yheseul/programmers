04a345514f012be09f8c6632e9d37d0f8eb97a67
yheseul <111976264+yheseul@users.noreply.github.com>
2024-08-14 22:20:34 +0900

    [level 0] Title: 모스부호 (1), Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,14 @@
+function solution(letter) {
+  let result = ''
+  morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+  }
+  letter.split(' ').map(code => {
+    Object.keys(morse).includes(code) ? result += morse[code] : result
+  })
+  return result
+}
