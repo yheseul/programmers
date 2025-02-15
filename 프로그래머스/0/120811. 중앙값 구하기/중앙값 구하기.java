30c65d3c03687aa54416fb7fe82b8142010985d5
yheseul <111976264+yheseul@users.noreply.github.com>
2025-02-15 23:12:39 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.55 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        int middle = array.length / 2;
+        return array[middle];
+    }
+}
