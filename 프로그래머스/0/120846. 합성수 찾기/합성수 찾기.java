b865d4907c8f63924f50abb7380690de053a36ca
yheseul <111976264+yheseul@users.noreply.github.com>
2025-02-16 23:36:55 +0900

    [level 0] Title: 합성수 찾기, Time: 0.13 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 4; i <= n; i++) {
+            int a = 0;
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0) a++;
+            }
+            if(a >=3) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
