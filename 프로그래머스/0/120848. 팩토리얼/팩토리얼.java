dc6505924c66eda27dc84fbc31bd9ad21450b64f
yheseul <111976264+yheseul@users.noreply.github.com>
2025-02-17 21:12:34 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        int factorial = 1;
+        
+        while (factorial <= n) {
+            i++;
+            factorial *= i;
+        }
+        return i - 1;
+    }
+}
